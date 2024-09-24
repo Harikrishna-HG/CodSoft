a79049d6f1cd96d1858adb30bc2b90886a869384
Harikrishna Gautam <93207808+Harikrishna-HG@users.noreply.github.com>
2024-09-24 07:15:30 +0545

    Update config.jsx

--- Job Board/client/src/pages/config.jsx
+++ Job Board/client/src/pages/config.jsx
@@ -1,8 +1,10 @@
 // src/config.js
 
-  apiUrl: "https://jobboard-backend-l8h1.onrender.com/job-cards",
 
+// Named export for apiUrl
+export const apiUrl = "https://jobboard-backend-l8h1.onrender.com/job-cards";
 
+// Default export for config
 const config = process.env.PORT && apiUrl;
 
 export default config;
